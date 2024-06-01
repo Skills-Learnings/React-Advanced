f66952f4b07f06c70b728ef52548a03d7d77354a
SahilGidwani <sahilgidwani98@gmail.com>
2024-06-01 14:52:48 +0530

    Update package-lock.json

--- src/Components/PropTypesExample/Parent.jsx
+++ src/Components/PropTypesExample/Parent.jsx
@@ -1,7 +1,7 @@
 import Child from "./Child"
 
-function Parent.jsx() {
+function Parent() {
   return <Child name="Sahil" age={23} address={{ street: "Hi", city: "city"}}>Hi I am Sahil</Child>
 }
 
-export default App
+export default Parent
